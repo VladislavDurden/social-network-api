38872973140503d1b0a52f9c85d3c1f0c63dfbdc
Vladislav <vpetrovskiy@tula.co>
2022-09-20 03:36:25 +0400

    first -v

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
